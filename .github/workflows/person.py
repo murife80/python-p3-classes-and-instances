71dea8807f03ef4bb71f51219c1504478f00f10b
melvin muchui <melvin.muchui1@student.moringaschool.com>
2025-05-24 10:50:11 +0300

    initial commit

---
+++ .github/workflows/person.py
@@ -0,0 +1,12 @@
+class person:
+    def talk(self):
+        print("Hello World !")
+
+    def walk(self):
+      print("The person is walking")
+     
+
+
+melvin= person()
+melvin.talk()
+melvin.walk()
---
+++ .github/workflows/test_dog.py
@@ -0,0 +1,13 @@
+class Dog:
+     def sit(self):
+          print("The dog is sitting")
+     def bark(self):
+          print("woof!")
+
+fido=Dog()
+fido.bark()
+
+snoopy=Dog()
+snoopy.bark()
+snoopy.sit()
+ 
